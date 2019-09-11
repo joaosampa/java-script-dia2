36be796298b77ab3ddb6e3a706f5892ab8798278
joaosampa <joao.profissional13@gmail.com>
2019-09-11 10:04:10 -0300

    intervalo aprendendo variaveis

---
+++ script.js
@@ -0,0 +1,43 @@
+// DECLARANDO VARIAVEIS
+
+var variavel1;
+let variavel2 = "estação hack";
+const variavel3 = 12;
+
+// ATRIBUIÇÃO
+
+var variavel4 = 12;
+let variavel5 = "estação hack";
+const variavel6 = true;
+
+// tipos de dados /  tipos primmitivios do js
+
+let exemplo;
+exemplo = "João"
+exemplo = 23;
+exemplo = 1,87;
+exemplo = true;
+exemplo = false;
+
+let saboresDePizza;
+
+saboresDePizza = ["brocolis", "baiana", "mussarela", "strogonoff", "portugesa"];
+
+let gordo = ["Joao", 19, true, "Taboao da serra", ["comida", "breja", "doces"] ];
+
+let demoday = {
+    sobrenome: "treta",
+    idade: 19,
+    viva: true,
+    bairro: "Paulista",
+    hobbies: ["trabalhar", "muito"] 
+};
+
+
+
+
+// length = mostra a quantidade de items dentro de uma lista
+// pop = exclui dados de uma lsita, ()vazio exclui o ultimo item, com algo dentro exlui o item especifico
+// find = n lembro
+// push = é igual a empurrar pra dentro da lista 
+// null nulo n ter nada
